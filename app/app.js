029759ca26d610663e14dade89581a5dfdd07494
Majid <majidrobo@gmail.com>
2017-04-08 02:30:09 +0430

    footer updated

--- app/app.js
+++ app/app.js
@@ -1,3 +1,11 @@
-angular.module('handGateModule').controller('handGateCtrl',['$scope','$state', function ($scope,$state) {
-
+angular.module('handGateModule').controller('handGateCtrl',['$scope','$state','$http', function ($scope,$state,$http) {
+    $scope.sendComment = function(comment) {
+        return $http({
+            method: 'POST',
+            url: 'http://api2.handgate.com/v1/SiteComment/',
+            data: comment
+        }).then(function () {
+            $scope.cm = {};
+        });
+    }
 }]);
